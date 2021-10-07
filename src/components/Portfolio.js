d93b89f590e3a76cd5f40139a306d03c8b8dce11
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2021-10-07 19:01:53 -0400

    added bootstrap header

---
+++ src/components/Portfolio.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Welcome({ onLoad, setOnLoad}){
+
+    return(
+        <div>
+            <header className="jumbotron jumbotron-fluid">
+                <h1 className="display-4">Nicholas Chin</h1>
+            </header>
+        </div>
+    )
+}
