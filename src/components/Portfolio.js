6e98c2f23bd2f8c385d63cc84c2a4917087b2a83
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2021-10-07 19:17:09 -0400

    added navbar

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,12 +1,43 @@
 import React from "react";
 
-export default function Welcome({ onLoad, setOnLoad}){
-
-    return(
-        <div>
-            <header className="jumbotron jumbotron-fluid">
-                <h1 className="display-4">Nicholas Chin</h1>
-            </header>
+export default function Welcome({ onLoad, setOnLoad }) {
+  return (
+    <div>
+      <header className="jumbotron jumbotron-fluid">
+        <h1 className="display-4">Nicholas Chin</h1>
+      </header>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <a className="nav-link" href="#aboutme">
+                About Me <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#work">
+                Work
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#contact">
+                Contact Me
+              </a>
+            </li>
+          </ul>
         </div>
-    )
+      </nav>
+    </div>
+  );
 }
