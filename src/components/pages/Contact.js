3f0ff2dda19c0e0dc91bf34d7116cf3868660468
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2021-10-11 01:24:16 -0400

    added title

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -5,31 +5,40 @@ function Contact() {
     // name
     // email
     // message
-
-    <form className="p-5">
-      <div className="form-group px-5 py-3">
-        <label for="userName">Name: </label>
-        <input
-          type="string"
-          className="form-control"
-          id="userName"
-          placeholder="John Smith"
-        ></input>
+    <div className="container-fluid contact">
+      <div className="row">
+        <div className="col-12">
+          <h2 className="text-center mt-4">Contact</h2>
+        </div>
       </div>
-      <div className="form-group px-5 py-3">
-        <label for="userEmail">Email: </label>
-        <input
-          type="email"
-          className="form-control"
-          id="userEmail"
-          placeholder="john.smith@email.com"
-        ></input>
-      </div>
-      <div className="form-group px-5 py-3">
-        <label for="emailMessage">Message: </label>
-        <textarea className="form-control" id="userEmailMessage" rows="3"></textarea>
-      </div>
-    </form>
+      <form className="p-5">
+        <div className="form-group px-5 py-3">
+          <label for="userName">Name: </label>
+          <input
+            type="string"
+            className="form-control"
+            id="userName"
+            placeholder="John Smith"
+          ></input>
+        </div>
+        <div className="form-group px-5 py-3">
+          <label for="userEmail">Email: </label>
+          <input
+            type="email"
+            className="form-control"
+            id="userEmail"
+            placeholder="john.smith@email.com"
+          ></input>
+        </div>
+        <div className="form-group px-5 py-3">
+          <label for="emailMessage">Message: </label>
+          <textarea className="form-control" id="userEmailMessage" rows="3"></textarea>
+        </div>
+        <button type="button" className="btn btn-success mx-5">
+          Submit
+        </button>
+      </form>
+    </div>
   );
 }
 
