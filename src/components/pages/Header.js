ce63e31c77a93d36d91827f11131eb61a715bc6f
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2021-10-10 22:16:54 -0400

    removed object destructuring

--- src/components/pages/Header.js
+++ src/components/pages/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Header({ currentPage, handlePageChange }) {
+function Header() {
   return (
     <header className="jumbotron jumbotron-fluid text-center text-light m-0">
       <h1 className="display-4 font-weight-normal text-center">Nicholas Chin</h1>
