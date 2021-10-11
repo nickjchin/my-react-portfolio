d9dd340c471b885631c23b69ff8f188cdabda7c4
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2021-10-10 22:14:00 -0400

    imported react and exported header function

--- src/components/pages/Header.js
+++ src/components/pages/Header.js
@@ -1,3 +1,11 @@
-<header className="jumbotron jumbotron-fluid text-center text-light m-0">
-<h1 className="display-4 font-weight-normal text-center">Nicholas Chin</h1>
-</header>
+import React from "react";
+
+function Header({ currentPage, handlePageChange }) {
+  return (
+    <header className="jumbotron jumbotron-fluid text-center text-light m-0">
+      <h1 className="display-4 font-weight-normal text-center">Nicholas Chin</h1>
+    </header>
+  );
+}
+
+export default Header;
