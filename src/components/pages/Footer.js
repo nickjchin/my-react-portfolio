607808039b46976d42499bc07a22a694bc992b44
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2021-10-10 22:18:15 -0400

    imported react and exported footer function

--- src/components/pages/Footer.js
+++ src/components/pages/Footer.js
@@ -1,30 +1,36 @@
-<footer className="bg-dark text-light mt-0 pt-3" id="contact">
-  {/* Social Media */}
-  <div className="container">
-    <div className="row">
-      <div className="col-12">
-        <ul className="text-center d-flex justify-content-around social">
-          <li className="d-inline">
-            <a href="https://www.linkedin.com/in/nicholaschin23/" target="_blank">
-              <i className="fab fa-linkedin-in fa-lg"></i>
-            </a>
-          </li>
-          <li className="d-inline">
-            <a href="https://www.github.com/nickjchin/" target="_blank">
-              <i className="fab fa-github fa-lg"></i>
-            </a>
-          </li>
-          <li className="d-inline">
-            <a href="https://twitter.com/NicholasJChin" target="_blank">
-              <i className="fab fa-twitter fa-lg"></i>
-            </a>
-          </li>
-        </ul>
+import React from "react";
+function Footer() {
+  return (
+    <footer className="bg-dark text-light mt-0 pt-3" id="contact">
+      {/* Social Media */}
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <ul className="text-center d-flex justify-content-around social">
+              <li className="d-inline">
+                <a href="https://www.linkedin.com/in/nicholaschin23/" target="_blank">
+                  <i className="fab fa-linkedin-in fa-lg"></i>
+                </a>
+              </li>
+              <li className="d-inline">
+                <a href="https://www.github.com/nickjchin/" target="_blank">
+                  <i className="fab fa-github fa-lg"></i>
+                </a>
+              </li>
+              <li className="d-inline">
+                <a href="https://twitter.com/NicholasJChin" target="_blank">
+                  <i className="fab fa-twitter fa-lg"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12"></div>
+          <p className="mx-auto mt-3">&copy;2021. Nicholas Chin. All rights reserved</p>
+        </div>
       </div>
-    </div>
-    <div className="row">
-      <div className="col-12"></div>
-      <p className="mx-auto mt-3">&copy;2021. Nicholas Chin. All rights reserved</p>
-    </div>
-  </div>
-</footer>;
+    </footer>
+  );
+}
+export default Footer;
