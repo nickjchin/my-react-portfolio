6e9eed605cba496a1b225473f017bdc498d5f6ad
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2021-10-10 21:49:32 -0400

    added page change handler and added resume

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,34 +1,37 @@
-import React, {useState} from "react";
-import Navbar from "./Navbar"
-import Header from "./pages/Header"
-import About from "./pages/About"
-import Projects from "./pages/Projects"
-import Contact from "./pages/Contact"
-import Footer from "./pages/Footer"
+import React, { useState } from "react";
+import Navbar from "Navbar";
+import Header from "./pages/Header";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import Footer from "./pages/Footer";
+import Resume from "./pages/Resume";
 import "../styles/Portfolio.css";
 
 export default function Portfolio() {
-    const [currentPage, setCurrentPage] = useState("About");
+  const [currentPage, setCurrentPage] = useState("About");
 
-    const renderPage = () =>{
-        if (currentPage === "About"){
-            return <About />;
-        } 
-        if (currentPage === "Projects"){
-            return <Projects />;
-        }
-        if (currentPage === "Contact"){
-            return <Contact />
-        };
-    
- const handlePageChange = (page) => setCurrentPage(page);
+  const renderPage = () => {
+    if (currentPage === "About") {
+      return <About />;
+    }
+    if (currentPage === "Projects") {
+      return <Projects />;
+    }
+    if (currentPage === "Contact") {
+      return <Contact />;
+    }
+    return <Resume />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
 
   return (
     <div>
-        <Header />
-
-        {renderPage()}
-        <Footer />
+      <Header />
+      <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+      <Footer />
     </div>
   );
 }
