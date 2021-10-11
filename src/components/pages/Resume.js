8b0b57a43c20eda31907d8f7deb42ca9234fbc7a
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2021-10-11 01:25:48 -0400

    added resume layout

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,7 +1,22 @@
 import React from "react";
 
-export default function Resume(){
-    return(
-
-    );
+export default function Resume() {
+  return (
+    <div className="container-fluid m-0">
+      <div className="row">
+        <div className="col-12">
+          <h2 className="text-center" id="resume">
+            R&eacute;sum&eacute;
+          </h2>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <h3 className="text-center" id="skills">
+              Skills
+            </h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
