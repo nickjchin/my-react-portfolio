39eb4ceba43ea66e8d821381fc324bd3742987e4
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2021-10-11 01:24:55 -0400

    changed styling

--- src/components/pages/Header.js
+++ src/components/pages/Header.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function Header() {
   return (
-    <header className="jumbotron jumbotron-fluid text-center text-light m-0">
-      <h1 className="display-4 font-weight-normal text-center m-0">Nicholas Chin</h1>
+    <header className="jumbotron jumbotron-fluid text-light m-0">
+      <h1 className="display-4 font-weight-normal text-center m-0 p-5">Nicholas Chin</h1>
     </header>
   );
 }
