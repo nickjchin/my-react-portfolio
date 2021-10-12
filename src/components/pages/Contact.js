078e6dd157b387838abc04f7d64ce5763be658af
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2021-10-12 18:00:46 -0400

    able to get error messages show underneath inputs. errors are showing even though there's no error

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,6 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { validateEmail } from "../../utils/helpers";
 
 function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [nameErrorMessage, setNameErrorMessage] = useState("");
+  const [emailErrorMessage, setEmailErrorMessage] = useState("");
+  const [messageErrorMessage, setMessageErrorMessage] = useState("");
+
+  const handleNameChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    // Based on the input type, we set the state of either email, username, and password
+    if (inputType === "name") {
+      setName(inputValue);
+    }
+
+    if (!name) {
+      setNameErrorMessage("Name is required");
+      // We want to exit out of this code block if something is wrong so that the user can correct it
+      return;
+    }
+  };
+  const handleEmailChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    // Based on the input type, we set the state of either email, username, and password
+    if (inputType === "email") {
+      setEmail(inputValue);
+    }
+
+    if (!validateEmail(email)) {
+      setEmailErrorMessage("Email is invalid");
+      // We want to exit out of this code block if something is wrong so that the user can correct it
+      return;
+    }
+  };
+  const handleMessageChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    // Based on the input type, we set the state of either email, username, and password
+    if (inputType === "email") {
+      setEmail(inputValue);
+    } else if (inputType === "name") {
+      setName(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
+
+    if (!message) {
+      setMessageErrorMessage("Message is invalid");
+      // We want to exit out of this code block if something is wrong so that the user can correct it
+      return;
+    }
+  };
+
   return (
     // name
     // email
@@ -19,7 +84,13 @@ function Contact() {
             className="form-control"
             id="userName"
             placeholder="John Smith"
+            onBlur={handleNameChange}
           ></input>
+          {nameErrorMessage && (
+            <div>
+              <p className="text-danger">{nameErrorMessage}</p>
+            </div>
+          )}
         </div>
         <div className="form-group px-5 py-3">
           <label for="userEmail">Email: </label>
@@ -28,12 +99,28 @@ function Contact() {
             className="form-control"
             id="userEmail"
             placeholder="john.smith@email.com"
+            onBlur={handleEmailChange}
           ></input>
+          {emailErrorMessage && (
+            <div>
+              <p className="text-danger">{emailErrorMessage}</p>
+            </div>
+          )}
         </div>
         <div className="form-group px-5 py-3">
           <label for="emailMessage">Message: </label>
-          <textarea className="form-control" id="userEmailMessage" rows="3"></textarea>
+          <textarea
+            className="form-control"
+            id="userEmailMessage"
+            rows="3"
+            onBlur={handleMessageChange}
+          ></textarea>
         </div>
+        {messageErrorMessage && (
+          <div>
+            <p className="text-danger">{messageErrorMessage}</p>
+          </div>
+        )}
         <button type="button" className="btn btn-success mx-5">
           Submit
         </button>
