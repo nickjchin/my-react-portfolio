f11251b0d2d6f7f2a3fa8938a30c778a0632e8cb
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2021-10-11 21:19:51 -0400

    added resume image with links to open or download

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,4 +1,6 @@
 import React from "react";
+import resume from "../../images/resume.PNG";
+import resumePDF from "../../documents/Nicholas-Chin's-Resume.pdf";
 
 export default function Resume() {
   return (
@@ -8,6 +10,15 @@ export default function Resume() {
           <h2 className="text-center" id="resume">
             R&eacute;sum&eacute;
           </h2>
+        </div>
+        <div className="resume-img text-center ">
+          <img src={resume} className="img-responsive w-25 border mx-auto d-block"></img>
+          <a href={resumePDF} target={"_blank"} className="resume">
+            Open
+          </a>
+          <a href={resumePDF} download>
+            Download
+          </a>
         </div>
         <div className="row">
           <div className="col-12">
